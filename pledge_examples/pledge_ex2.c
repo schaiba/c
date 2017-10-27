3575e30b4b8fd9468bb1dee1b9cac2cbcfa799df
schaiba <schaiba@gmail.com>
2017-10-27 18:56:41 +0300

    Added hint to exercises in comments

--- pledge_examples/pledge_ex2.c
+++ pledge_examples/pledge_ex2.c
@@ -23,17 +23,20 @@ int main()
 		perror("stat");
 		exit(1);
 	}
-	int offset = 2;//make sure you have something in your file, with zero-length files the below condition will fail & exit everytime. Exercise 1 : Why?
+	int offset = 2;//make sure you have something in your file, with zero-length files the below condition will fail & exit 
+	//everytime. Exercise 1 : Why?
 	if (offset < 0 || offset > fbuf.st_size-1) 
 	{
 		fprintf(stderr, "mmapdemo: offset must be in the range 0-%d\n", (int)fbuf.st_size-1);
 		exit(1);
 	}
 	int pagesize = getpagesize();
-	if(pledge("prot_exec", NULL) == -1) //The program will work without the call to pledge. Exercise 2 : Why? Exercise 3 : What happens if we move this block _below_ the call to mmap() ?
+	if(pledge("prot_exec", NULL) == -1) //The program will work without the call to pledge. Exercise 2 : Why? Exercise 3 : What happens if we move this 
+		//block _below_ the call to mmap() ?
 		err(1, "pledge");
 
-	char *data = mmap(NULL, pagesize, PROT_READ & PROT_EXEC, MAP_PRIVATE, fd, 0); //Exercise 4 : Would you use different PROT_ flags? Why?
+	char *data = mmap(NULL, pagesize, PROT_READ & PROT_EXEC, MAP_PRIVATE, fd, 0); //Exercise 4 : Would you use different 
+	//PROT_ flags? Why? HINT: mmap(2) manpage
 	if (data == (caddr_t)(-1))
        	{
 		perror("mmap");
